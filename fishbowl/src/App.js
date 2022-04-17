e50916ba8b27626f32bc87046ecbacd52f955122
Dennis Li <62808150+lidennis610@users.noreply.github.com>
2022-04-16 20:17:10 -0700

    Update App.js

--- fishbowl/src/App.js
+++ fishbowl/src/App.js
@@ -4,6 +4,20 @@ import { Route, Routes } from 'react-router-dom';
 import SignIn from './Signin';
 import SignUp from './Signup'
 import PrivateRoute from './PrivateRoute'
+import React, { useState } from "react";
+import { Dropdown, Option } from "./Dropdown";
+
+export default function App() {
+  const [optionValue, setOptionValue] = useState("");
+  const handleSelect = (e) => {
+    console.log(e.target.value);
+    setOptionValue(e.target.value);
+  };
+
+  return (
+
+  );
+}
 
 function App() {
   return (
@@ -13,6 +27,26 @@ function App() {
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
       </Routes>
+    </div>
+
+    <div>
+      <h1>Which service are you interested in?</h1>
+      <Dropdown
+        formLabel="Choose a service"
+        buttonText="Send form"
+        onChange={handleSelect}
+        action="/"
+      >
+        <Option selected value="days" />
+        <Option value="Monday" />
+        <Option value="Tuesday" />
+        <Option value="Wednesday" />
+        <Option value="Thursday" />
+        <Option value="Friday" />
+        <Option value="Saturday" />
+        <Option value="Sunday" />
+      </Dropdown>
+      <p>You selected {optionValue} </p>
     </div>
   );
 }
