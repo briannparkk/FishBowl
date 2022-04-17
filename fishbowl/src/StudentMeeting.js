1d9fa7ea68f49874f5a6ccb96b6e155f285959af
Akhil Manthina <akhilmanthina@gmail.com>
2022-04-17 03:49:41 -0700

    added realtime tracy >:)

--- fishbowl/src/StudentMeeting.js
+++ fishbowl/src/StudentMeeting.js
@@ -2,13 +2,24 @@ import React from 'react'
 import QuestionBox from './QuestionBox'
 import { useState, setState } from 'react';
 import YoutubeEmbed from './YoutubeEmbed';
+import { configureAbly, useChannel } from "@ably-labs/react-hooks";
+
+configureAbly({ key: "oyCG5A.V_cCpQ:m5A20ynS4h1Y3tgxRcTPF0x4Rhfgr172EW3WpSmajZ4"});
 
 export default function StudentMeeting() {
     const [questions, setQuestions] = useState([]);
+
+    const [channel] = useChannel("test", (question) => {
+        setQuestions((prev) => [...prev, question.data.value]);
+        console.log(question);
+        console.log(questions);
+    });
+
     function askQuestion() {
         let question = document.getElementById("this-question").value;
         console.log(question);
-        setQuestions([...questions, question]);
+        console.log(typeof(question));
+        channel.publish("test_message", { value: question });
         document.getElementById("this-question").value = "";
     }
     return (
@@ -21,8 +32,8 @@ export default function StudentMeeting() {
             <div className='student-question-half'>
                 <div style={{textAlign: 'center', color: 'white', paddingTop: '20px', fontSize: '28px'}}>questions</div> <br/>
                 <div>
-                    {questions.map((value) => (
-                        <QuestionBox text={value}/> 
+                    {questions.map((value, index) => (
+                        <QuestionBox key = {index} text={value}/> 
                     ))}
                     <br />
                 </div>
